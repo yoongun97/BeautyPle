a3bb8fe5e8d2a5e018aa5a362e464fe73466a9ab
Cho Yoongeon <tesung5470@naver.com>
2023-08-11 22:10:29 +0900

    댓글창 UI 구현

---
+++ src/components/comment/StyledComment.js
@@ -0,0 +1,45 @@
+import { styled } from "styled-components";
+
+export const StInputBox = styled.div`
+  margin-bottom: 10px;
+`;
+
+export const StCommentInput = styled.input`
+  width: 85%;
+  height: 35px;
+  border: 1px solid lightgray;
+  border-radius: 12px;
+`;
+
+export const StInputBtn = styled.button`
+  height: 40px;
+  background-color: white;
+  border: 1px solid lightgray;
+  border-radius: 12px;
+  margin: 0 10px 0 10px;
+  cursor: pointer;
+  &:hover {
+    background-color: #83925a;
+    color: white;
+  }
+`;
+
+export const StCommentBox = styled.div`
+  background-color: white;
+  border: 1px solid lightgray;
+  border-radius: 12px;
+  height: 200px;
+  overflow: auto;
+  padding: 15px;
+`;
+
+export const StCommentCard = styled.div`
+  display: flex;
+  border-bottom: 1px solid lightgray;
+`;
+
+export const StComment = styled.p``;
+
+export const StCommentAuthor = styled.p`
+  margin-left: auto;
+`;
