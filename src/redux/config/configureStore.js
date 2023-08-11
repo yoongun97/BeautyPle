6c975d3a030c386ebae9f6f5124ff38bea61bc51
jinny <ertmiu96@gmail.com>
2023-08-11 09:33:10 +0900

    추가페이지

--- src/redux/config/configureStore.js
+++ src/redux/config/configureStore.js
@@ -1,10 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
-import subCategory from "../modules/postsSlice"
+import subCategory from "../modules/postsSlice";
+import user from "../modules/usersSlice";
+
 // 데이터 중앙 저장소 만들기
 const store = configureStore({
   reducer: {
     // 2. 다른 곳에서 사용할 수 있게 configureStore에 넣어주기
     subCategory,
+    user: user.reducer,
   },
 });
 
