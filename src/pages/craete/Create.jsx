9dd6eedb99a0cb8082aea98e882766f9cd9ede26
Cho Yoongeon <tesung5470@naver.com>
2023-08-11 16:13:38 +0900

    create 코드 merge

--- src/pages/craete/Create.jsx
+++ src/pages/craete/Create.jsx
@@ -20,17 +20,23 @@ import {
 } from "./StyledCreate";
 
 export default function Create() {
-  //셀렉트 관련 -추후수정
   const upperOptions = ["제품추천", "꿀팁공유"];
   const lowerOptions = {
     제품추천: ["올인원", "기초화장"],
     꿀팁공유: ["생활 꿀팁", "쇼핑 꿀팁"],
   };
-
+  const user = useSelector((state) => state.User);
+  const navigate = useNavigate();
+  const queryClient = useQueryClient();
   const [isUpperOpen, setIsUpperOpen] = useState(false);
   const [isLowerOpen, setIsLowerOpen] = useState(false);
   const [selectedUpperOption, setSelectedUpperOption] = useState(null);
   const [selectedLowerOption, setSelectedLowerOption] = useState(null);
+  const [selectedFile, setSelectedFile] = useState(null);
+  const [inputs, setInputs] = useState({
+    title: "",
+    content: "",
+  });
 
   // select바 action
   const handleUpperOptionClick = (option) => {
@@ -41,35 +47,20 @@ export default function Create() {
   };
   const handleLowerOptionClick = (option) => {
     setSelectedLowerOption(option);
-    setInputs({
-      ...inputs,
-      selectedUpperOption,
-      selectedLowerOption,
-    });
     setIsLowerOpen(false); // 하위 카테고리 선택 시 상위 카테고리 닫기
   };
-
-  const user = useSelector((state) => state.User);
-  const navigate = useNavigate();
-  const [inputs, setInputs] = useState({
-    title: "",
-    content: "",
-    selectedUpperOption: "", // 상위 카테고리 선택값 추가
-
-    selectedLowerOption: "", // 하위 카테고리 선택값 추가
-  });
 
   const changeHandler = (e) => {
     const { value, name } = e.target;
     setInputs({
       ...inputs,
       [name]: value,
+      selectedUpperOption,
+      selectedLowerOption,
     });
   };
 
   // 추가하기 기능
-  const queryClient = useQueryClient();
-
   const mutation = useMutation(
     async (newPost) => {
       await api.post("posts", newPost);
@@ -86,6 +77,7 @@ export default function Create() {
       author: user.email,
       uid: user.id,
       id: uuid(),
+      attachment: selectedFile,
     };
     mutation.mutate(newPost);
     navigate("/");
@@ -96,7 +88,7 @@ export default function Create() {
       <StCreateForm
         onSubmit={(e) => {
           e.preventDefault();
-          console.log("제출!");
+          addButton();
         }}
       >
         <StCreateTitle
@@ -165,18 +157,23 @@ export default function Create() {
           placeholder="내용"
         />
         <StFileBox>
-          <StFileLink type="text" placeholder="파일링크" />
+          <StFileLink
+            value={selectedFile ? selectedFile.name : ""}
+            type="text"
+            placeholder="파일링크"
+          />
           <StFileBtn htmlFor="attachment">첨부파일</StFileBtn>
-          <input type="file" id="attachment" style={{ display: "none" }} />
+          <input
+            type="file"
+            id="attachment"
+            style={{ display: "none" }}
+            onChange={(e) => {
+              setSelectedFile(e.target.files[0]);
+            }}
+          />
         </StFileBox>
 
-        <StCreateBtn
-          onClick={() => {
-            addButton();
-          }}
-        >
-          추가하기
-        </StCreateBtn>
+        <StCreateBtn>추가하기</StCreateBtn>
       </StCreateForm>
     </>
   );
